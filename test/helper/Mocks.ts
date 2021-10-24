4e58b8d01831da25de8bd512f04e3c849ee6569e
belanEu <evgeniy.belan.97@mail.ru>
2021-10-24 14:06:02 +0300

    созданы моки и стабы

---
+++ test/helper/Mocks.ts
@@ -0,0 +1,27 @@
+import { AxiosResponse } from "axios";
+import { ExampleApi, CartApi } from "../../src/client/api";
+import { ProductShortInfo, Product, CheckoutFormData, CartState, CheckoutResponse } from "../../src/common/types";
+import { getProductStub, getProductsShortInfoStub, OrderStub } from "./Stubs";
+
+export class ExampleApiMock extends ExampleApi {
+    shortInfoProducts: ProductShortInfo[];
+
+    constructor(amountOfProducts: number) {
+        super('/');
+        this.shortInfoProducts = getProductsShortInfoStub(amountOfProducts);
+    }
+
+    async getProducts() {
+        return {data: this.shortInfoProducts} as AxiosResponse<ProductShortInfo[]>;
+    }
+
+    async getProductById(id: number) {
+        return {data: getProductStub(id)} as AxiosResponse<Product>;
+    }
+
+    async checkout(form: CheckoutFormData, cart: CartState) {
+        return {data: {id: 3}} as AxiosResponse<CheckoutResponse>;
+    }
+}
+
+export class CartApiMock extends CartApi {}
