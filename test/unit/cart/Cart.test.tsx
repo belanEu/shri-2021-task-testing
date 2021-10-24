18488ce095344ac236621547e5f69312aed16115
belanEu <evgeniy.belan.97@mail.ru>
2021-10-24 17:44:04 +0300

    проверка страницы Корзина

---
+++ test/unit/cart/Cart.test.tsx
@@ -0,0 +1,106 @@
+/**
+ * @jest-environment jsdom
+ */
+ import React from 'react'
+ import { Provider } from "react-redux";
+ import { Router } from 'react-router';
+ import { createMemoryHistory } from 'history';
+ import { render, waitFor, within, screen } from '@testing-library/react';
+ import '@testing-library/jest-dom'
+ 
+ import { initStore, addToCart } from "../../../src/client/store";
+ import { CartApi } from '../../../src/client/api';
+ import { ExampleApiMock } from "../../helper/Mocks";
+import { Application } from '../../../src/client/Application';
+import userEvent from '@testing-library/user-event';
+import { getProductStub } from '../../helper/Stubs';
+import {CartItem} from '../../../src/common/types';
+import { Cart } from '../../../src/client/pages/Cart';
+
+describe('проверка страницы Shopping cart', () => {
+    const AMOUNT_OF_PRODUCTS = 5;
+    let history = createMemoryHistory({
+        initialEntries: ['/cart'],
+        initialIndex: 0
+    });
+    let store = initStore(new ExampleApiMock(AMOUNT_OF_PRODUCTS), new CartApi);
+
+    test('кол-во неповторяющихся товаров в шапке рядом со ссылкой на корзину', () => {
+        render((
+            <Router history={history}>
+                <Provider store={store}>
+                    <Application />
+                </Provider>
+            </Router>
+        ));
+
+        // await waitFor(() => store.subscribe);
+
+        // трижды добавляется первый товар
+        store.dispatch(addToCart(getProductStub(0)));
+        store.dispatch(addToCart(getProductStub(0)));
+        store.dispatch(addToCart(getProductStub(0)));
+
+        // дважды добавляется второй товар
+        store.dispatch(addToCart(getProductStub(1)));
+        store.dispatch(addToCart(getProductStub(1)));
+
+        const amountOfChosenProducts = Object.keys(store.getState().cart).length;
+        expect(amountOfChosenProducts).toEqual(2);
+        expect(screen.getByRole('link', {name: `Cart (${amountOfChosenProducts.toString()})`})).toBeInTheDocument();
+    });
+
+    describe('проверка компонента Cart', () => {
+        beforeEach(() => {
+            history = createMemoryHistory({
+                initialEntries: ['/cart'],
+                initialIndex: 0
+            });
+            store = initStore(new ExampleApiMock(AMOUNT_OF_PRODUCTS), new CartApi);
+            
+            render((
+                <Router history={history}>
+                    <Provider store={store}>
+                        <Cart />
+                    </Provider>
+                </Router>
+            ));
+        });
+
+        test('отображена таблица с выбранными продуктами', () => {
+            expect(store.getState().cart).toBeDefined();
+    
+            expect(
+                Object.keys(store.getState().cart).every(id => {
+                    const productData: CartItem = store.getState().cart[+id];
+                    const tableItem = screen.getByTestId(id);
+    
+                    return within(tableItem).queryAllByText(productData.name)
+                        && within(tableItem).queryAllByText(productData.count)
+                        && within(tableItem).queryAllByText(`$${productData.price}`)
+                        && within(tableItem).queryAllByText(`$${productData.count * productData.price}`);
+                })
+            ).toBeTruthy();
+    
+            const totalPrice = Object.values(store.getState().cart)
+                .reduce((sum, { count, price }) => sum + count * price, 0);
+            expect(screen.queryByText(`$${totalPrice}`)).toBeInTheDocument();
+        });
+    
+        test('очистка корзины по нажатию на Clear shopping cart', () => {
+            const clearButton = screen.getByRole('button', {name: /Clear shopping cart/i});
+            expect(clearButton).toBeInTheDocument();
+    
+            const leftClick = {button: 0};
+    
+            userEvent.click(clearButton, leftClick);
+    
+            expect(Object.keys(store.getState().cart).length).toEqual(0);
+        });
+    
+        test('пустая корзина. есть ссылка на каталог', () => {    
+            expect(screen.queryByText(/Cart is empty. Please select products in the/i)).toBeInTheDocument();
+            expect(screen.queryByRole('link', {name: /catalog/i})).toBeInTheDocument();
+        });
+    });
+});
