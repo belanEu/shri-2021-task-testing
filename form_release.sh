abb6cc721340b44fc6ce2b1ebc4d946741b8d6f1
belanEu <evgeniy.belan.97@mail.ru>
2021-10-31 16:16:56 +0300

    создание задачи

--- form_release.sh
+++ form_release.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-lastTagVersion=$(git tag -l *-release | head -1)
+lastTagVersion=$(git describe --abbrev=0 --match *-release)
 tagInfo=$(git show "$lastTagVersion")
 cutWord="commit"
 
-echo "${tagInfo%%$cutWord*}"
+tagData="${tagInfo%%$cutWord*}"
+
+echo "$tagData"
+
+curl -X POST \
+    -H 'Content-Type: application/json' \
+    -H 'Authorization: OAuth AQAAAAA7IE6_AAd46F8KihtUREqBrRXr7eV_5yU' \
+    -H 'X-Org-Id: 6461097' \
+    --data '{"queue":"TMP","summary":"Release tag '"$lastTagVersion"'"}' \
+    https://api.tracker.yandex.net/v2/issues/
